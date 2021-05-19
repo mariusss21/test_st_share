0d9e5f70cc496a1d535b9dafa30e91013a0221a9
mariusss21 <39649854+mariusss21@users.noreply.github.com>
2021-05-19 00:08:17 -0300

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -2,19 +2,44 @@
 import streamlit as st
 import pandas as pd
 
-DATA_URL = "https://drive.google.com/file/d/1pz0onHrxh5pJF9_coSCYcMGQE5Z6gVJR/view?usp=sharing"
+DATA_URL = "data.csv"
 
-@st.cache
+#@st.cache
 def load_data():
 	data = pd.read_csv(DATA_URL)
 	return data
 
 df = load_data()
+colunas = df.columns
+
+st.write(df.head())
 
 st.sidebar.title("Escolha a ação desejada")
-if st.sidebar.checkbox("Inserir ocorrência 5 Porquês"):
-	df = df.append(list(range(0,32)), ignore_index=True)
-	st.write(df.head())
-	df.to_csv(DATA_URL)
+#if st.sidebar.checkbox("Inserir ocorrência 5 Porquês"):
 
+lista = []
+submitted1=False
+
+with st.form('Form1'):
+	lista.append(st.date_input('Data da ocorrência'))
+	lista.append(st.selectbox('Selecione o turno', ['Turno A', 'Turno B', 'Turno C']))
+	lista.append(st.selectbox('Selecione o departamento', ['Engenharia', 'Automação']))
+	lista.append(st.selectbox('Selecione a linha', ['L571', 'L572', 'L581', 'Utilidades']))
+	if lista[3] == 'L571':
+    		lista.append(st.selectbox('Selecione o equipamento', ['1', '2', '3', '4']))
+	elif lista[3] == 'L572':
+    		lista.append(st.selectbox('Selecione o equipamento', ['5', '6', '7', '8']))
+	else:
+    		lista.append(st.selectbox('Selecione o equipamento', ['5', '6']))
+
+	lista.append(st.slider(label='Select intensity', min_value=0, max_value=100))
+	submitted1 = st.form_submit_button('Enviar 5 Porquês')
+
+if submitted1:
+	df2 = pd.DataFrame(lista).T
+	df2.columns = colunas
+	df = pd.concat([df, df2], join='inner', ignore_index=True)
+	df.to_csv(DATA_URL, index=False)
+
+st.write(df.head())
 
