6e776c4ec9702241f25bf95ee457473770062f08
mariusss21 <39649854+mariusss21@users.noreply.github.com>
2021-05-21 15:16:40 -0300

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -137,12 +137,14 @@ if analisar:
 					att_verificado = {}
 					att_verificado['verificado'] = 'sim'
 					db.collection("5porques_2").document(row['document']).update(att_verificado)
+					row['verificado'] = 'sim'
 					caching.clear_cache()
 					
 				if abrir_ocorrencia:
 					att_verificado = {}
 					att_verificado['verificado'] = 'não'
 					db.collection("5porques_2").document(row['document']).update(att_verificado)
+					row['verificado'] = 'não'
 					caching.clear_cache()
 					
 				
