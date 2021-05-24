b1a29b34511ff9365521b281fbb5309334ef105b
mariusss21 <39649854+mariusss21@users.noreply.github.com>
2021-05-24 10:02:23 -0300

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -227,14 +227,14 @@ if analisar:
 		filtrado = filtrado[filtrado['responsável identificação'] == responsavel]
 		
 	#st.text('Selecione o Gestor')
-	gestor = st.selectbox("Selecione o status", list(filtrado['gestor'].drop_duplicates()))
+	gestor = st.selectbox("Selecione o gestor", list(filtrado['gestor'].drop_duplicates()))
 	if gestor is not None and (str(gestor) != 'nan'):
 		filtrado = filtrado[filtrado['gestor'] == gestor]	
 	
 	#st.text('Selecione o status da ocorrência')
 	status = st.selectbox("Selecione o status", list(filtrado['status'].drop_duplicates()))
 	if status is not None and (str(status) != 'nan'):
-		filtrado = filtrado[filtrado['gestor'] == status]	
+		filtrado = filtrado[filtrado['status'] == status]	
 	
 	st.write(filtrado[['data', 'responsável identificação', 'gestor', 'status', 'turno', 'linha', 'equipamento']])
 	detalhar_todas = st.checkbox("Detalhar todas as ocorrências")
