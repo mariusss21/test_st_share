839ece6df44395701f38fdd98e2eec5b65b79639
mariusss21 <39649854+mariusss21@users.noreply.github.com>
2021-05-24 11:39:07 -0300

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -138,7 +138,7 @@ def formulario():
 		new_d = {str(key): str(value) for key, value in keys_values}
 		for key, value in new_d.items():
 			if (value == '') or value == '[]':
-				value = 'Não preenchido'
+				new_d[key] = 'Não preenchido'
 		doc_ref = db.collection("5porques_2").document()
 		doc_ref.set(new_d)
 		
