66d2df59e1e1320d535d7b9c9402f090ce49b2aa
mariusss21 <39649854+mariusss21@users.noreply.github.com>
2021-05-28 10:52:09 -0300

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -98,7 +98,7 @@ def download(df):
 	in:  dataframe
 	out: href string
 	"""
-	csv = df.to_csv(index=False)
+	csv = df.to_excel(index=False)
 	b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
 	href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
 	return href
