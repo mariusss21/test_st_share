8113106d973576f6ef3ded16cdaddf3bbe353249
mariusss21 <39649854+mariusss21@users.noreply.github.com>
2021-05-28 10:45:19 -0300

    Update demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -94,13 +94,14 @@ def send_email(to, atividade, documento, comentario):
 ######################################################################################################
 		
 def download(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+	"""Generates a link allowing the data in a given panda dataframe to be downloaded
+	in:  dataframe
+	out: href string
+	"""
+	csv = df.to_csv(index=False)
+	b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+	href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+	return href
 
 ######################################################################################################
                                            #Função para leitura do banco (Firebase)
